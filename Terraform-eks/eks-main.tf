c1e4c1ee3f00ebe590a0fc0bb8e7756aa4b1a30d
dikshaturkar2022-sys <dikshaturkar2022@gmail.com>
2026-04-03 13:46:34 +0530

    updated tf files

--- Terraform-eks/eks-main.tf
+++ Terraform-eks/eks-main.tf
@@ -22,7 +22,7 @@ data "aws_subnets" "default_subnets" {
 # -------------------------
 
 resource "aws_iam_role" "eks_role" {
-  name = "eks-terraform-1"
+  name = "my-eks-terraform"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -50,8 +50,8 @@ resource "aws_eks_cluster" "eks_cluster" {
   role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
-    subnet_ids             = data.aws_subnets.default_subnets.ids
-    endpoint_public_access = true
+    subnet_ids              = data.aws_subnets.default_subnets.ids
+    endpoint_public_access  = true
   }
 
   depends_on = [
@@ -64,7 +64,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 # -------------------------
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role-terraform-1"
+  name = "my-eks-node-role-terraform"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -99,12 +99,12 @@ resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
 
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group"
+  node_group_name = "my-eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = data.aws_subnets.default_subnets.ids
 
   # ⚠️ IMPORTANT FIX (recommended)
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
 
   scaling_config {
     desired_size = var.desired_nodes
