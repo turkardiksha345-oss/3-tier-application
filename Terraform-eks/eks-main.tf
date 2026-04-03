77c82f8299871cb3fdae8e8f25ede11c3ab313c6
dikshaturkar2022-sys <dikshaturkar2022@gmail.com>
2026-04-03 11:25:19 +0530

    updates done

--- Terraform-eks/eks-main.tf
+++ Terraform-eks/eks-main.tf
@@ -104,7 +104,7 @@ resource "aws_iam_role_policy_attachment" "node_ecr_policy" {
 
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group-1"
+  node_group_name = "eks-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = data.aws_subnets.default_subnets.ids
 
